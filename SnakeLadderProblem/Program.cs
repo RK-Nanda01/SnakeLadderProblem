fb9bc2074253dc76a7abc87e5e45e14dd38e7087
R.K Nanda <rk@RK-s-MacBook-Air.local>
2023-07-07 22:00:32 +0530

    [RK Nanda] Adding a class User

--- SnakeLadderProblem/Program.cs
+++ SnakeLadderProblem/Program.cs
@@ -3,7 +3,12 @@ class Program
 {
     static void Main(string[] args)
     {
+        //Start
         Console.WriteLine("Snake Ladder Problem");
+
+        //UC1
+        //u1 starts from position 0
+        User u1 = new User();
     }
 }
 
---
+++ SnakeLadderProblem/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace SnakeLadderProblem
+{
+	public class User
+	{
+		private int startPosition;
+		private int currentPosition;
+
+		public User()
+		{
+			startPosition = 0;
+			currentPosition = 0;
+		}
+	}
+}
+
